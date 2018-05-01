1d33019b455aa0d1ddbf4fe3556cd34c5a3245a7
Tina <tinaliu@illinois.edu>
2018-05-01 16:38:28 -0500

    Confusing

--- app/src/main/java/cs125_illinois_students/github/com/networks/ShowDecrypt.java
+++ app/src/main/java/cs125_illinois_students/github/com/networks/ShowDecrypt.java
@@ -1,4 +1,19 @@
 package cs125_illinois_students.github.com.networks;
 
-public class ShowDecrypt {
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ShowDecrypt extends Encrypt {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.showdecrypt);
+        TextView privateView = findViewById(R.id.showDecrypt);
+        try {
+            CharSequence seq = new String(decryptArray, "US-ASCII");
+            privateView.setText(seq);
+        } catch (Exception e) {
+        }
+    }
 }
+
